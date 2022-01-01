934f7eb78babbc06c38343e380292369179601be
AhmadRyahnah <ahmad.riahna@gmail.com>
2022-01-01 05:12:08 +0200

    ...

--- my-app/src/Components/Landing Page/Hero.js
+++ my-app/src/Components/Landing Page/Hero.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import './Hero.css'
-import HeroImage from './hero.jpg'
+
 const Hero = () => {
     return (
         <div className="heroContainer">
             <div className="heroImage">
-                <span> BUILD YOUR PC</span>
+                <div className="btnOne">
+                    <h1> lorem lorem lorem</h1>
 
-                <Link to="/shop"><button>SHOP NOW</button></Link>
+                    <Link to="/shop"><button>SHOP NOW</button></Link>
+                </div>
             </div>
         </div>
     )
